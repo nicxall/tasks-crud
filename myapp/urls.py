eacb7056435b74e7ba86f7403ec0145ff56405c8
juan <cuentapersonaluni@gmail.com>
2024-04-20 04:25:35 +0200

    "Se modifico urls"

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from .TaskOperations import (TaskCreate, TaskList, TaskDetail, DeleteTask)
 
 urlpatterns = [
 	path('home/', TemplateHome.as_view(), name = 'home'),
-	path('signin/', SigninUser.as_view(), name = "signin"),
+	path('/', SigninUser.as_view(), name = "signin"),
 	path('signup/', Signup.as_view(), name = 'signup'),
 	path('logout/', SessionCloseUser, name = 'logout'),
 	#endpoint de task
