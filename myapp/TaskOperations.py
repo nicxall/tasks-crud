6ae0750091af8b4f3cb4b53af3faa95cdb33e68e
juan <cuentapersonaluni@gmail.com>
2024-04-26 05:20:55 +0200

    "Se agrego mas seguridad a las vistas"

--- myapp/TaskOperations.py
+++ myapp/TaskOperations.py
@@ -23,13 +23,12 @@ def get_user_tasks(request):
     return TaskModel.objects.filter(user=request.user).order_by('-created', 'title')
 
 # Vistas
-@login_required
 class TaskCreate(View):
     template_task_create = "taskcreate.html"
-
+    @login_required
     def get(self, request):
         return render(request, self.template_task_create)
-
+    @login_required
     def post(self, request):
         try:
             task_form = TaskForm(request.POST)
@@ -46,10 +45,9 @@ class TaskCreate(View):
                 messages.error(request, "Ocurrió un error al crear la tarea")
             return render(request, self.template_task_create, {'form': task_form})
 
-@login_required
 class TaskList(View):
     template_task_list = "tasklist.html"
-
+    @login_required
     def get(self, request):
         try:
             tasks = get_user_tasks(request)
@@ -57,12 +55,13 @@ class TaskList(View):
         except Exception as e:
             print(f'El error es: {e}')
 
-@login_required
+
 class TaskDetail(View):
+    @login_required
     def get(self, request, pk):
         task = get_object_or_404(get_user_tasks(request), pk=pk)
         return render(request, 'taskdetail.html', {'task': task})
-
+    @login_required
     def post(self, request, pk):
         task = get_object_or_404(get_user_tasks(request), pk=pk)
         task_form = TaskForm(request.POST, instance=task)
