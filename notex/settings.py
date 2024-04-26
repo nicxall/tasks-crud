3601b752258dd11cbc80fd76148434070c25749d
juan <cuentapersonaluni@gmail.com>
2024-04-26 05:08:44 +0200

    Se agrego para auth con facebook

--- notex/settings.py
+++ notex/settings.py
@@ -164,3 +164,5 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    
